42cde079d1be509efbe5ea0e8af8071f84d41696
« Christian » <christianbastide95@gmail.com>
2024-06-22 20:43:01 +0200

    Fix

--- functions/index.js
+++ functions/index.js
@@ -97,86 +97,87 @@ exports.Man_OOTD_Country_France_3_postTOswipe = functions.pubsub.topic('topic-Ma
       functions.logger.error("Error adding posts from posts_man to swipe_man:", error);
     }
   });
+
+  exports.Man_OOTD_Country_France_4_deletePosts = functions.pubsub.topic('topic-Man_OOTD_Country_France_3_postTOswipe').onPublish(async () => {
+    try {
+      const locationPath = 'France';
+      const postsSnapshot = await admin.firestore().collection(`posts_man/${locationPath}/posts`).get();
   
-// const functions2 = require("firebase-functions");
-
-exports.generateClonedPosts = functions.pubsub.schedule('every 24 hours').onRun(async () => {
-  const postIds = [
-    "HP9bmPGps1NQufCy22SD",
-    "IJDGbsUdowDHftsE1tGZ",
-    "WRuRT0XZ3MWYSK04otkQ",
-    "u5J17e3m5PfXxNuOcNfR"
-  ];
-
-  console.log('generateClonedPosts function started');
-
-  try {
-    // Récupérer les détails des posts existants
-    console.log('Fetching details of existing posts');
-    const posts = await Promise.all(
-      postIds.map(async (postId) => {
-        const querySnapshot = await admin.firestore().collectionGroup('posts').where('__name__', '==', postId).get();
-        if (!querySnapshot.empty) {
-          const postDoc = querySnapshot.docs[0];
-          console.log(`Post found with ID: ${postId}`);
-          return { ...postDoc.data(), id: postId, authorRef: postDoc.ref };
-        } else {
-          console.warn(`No post found with ID: ${postId}`);
-        }
-        return null;
-      })
-    );
-
-    // Filtrer les posts null
-    const validPosts = posts.filter(post => post !== null);
-
-    if (validPosts.length === 0) {
-      console.warn('No valid posts found to clone');
-      return null;
-    }
-
-    for (let i = 0; i < 10; i++) {
-      const originalPost = validPosts[i % validPosts.length]; // Récupérer un post en boucle
-      const newPostId = admin.firestore().collection("posts").doc().id;
-
-      const newPost = {
-        ...originalPost,
-        caption: `${originalPost.caption} (cloned #${i + 1})`,
-        likes: 0,
-        date: admin.firestore.FieldValue.serverTimestamp(),
-      };
-
-      // Déterminer le chemin de la collection basée sur le genre et la localisation
-      const genderPath = originalPost.selectedGender === 'Masculin' ? 'posts_man' : 'posts_woman';
-      const locationPath = _getLocationPath(originalPost);
-
-      const locationPostRef = admin.firestore().collection(`${genderPath}/${locationPath}/posts`).doc(newPostId);
-
-      await locationPostRef.set({
-        post_ref: originalPost.authorRef,
-        date: newPost.date,
+      const batch = admin.firestore().batch();
+      postsSnapshot.forEach(doc => {
+        const docRef = admin.firestore().collection(`posts_man/${locationPath}/posts`).doc(doc.id);
+        batch.delete(docRef);
       });
-
-      console.log(`Post cloned and added to ${locationPostRef.path}`);
+  
+      await batch.commit();
+      functions.logger.info('Posts deleted successfully from posts_man');
+  
+      const messageId = await pubsub.topic('topic-Man_OOTD_Country_France_4_deletePosts').publish(Buffer.from('Posts deleted'));
+      functions.logger.info(`Message ${messageId} published.`);
+  
+    } catch (error) {
+      functions.logger.error("Error deleting posts from posts_man:", error);
     }
-
-    console.log('10 cloned posts created successfully');
-  } catch (error) {
-    console.error('Error creating cloned posts:', error);
-  }
-});
-
-// Fonction utilitaire pour obtenir le chemin de localisation
-function _getLocationPath(post) {
-  let locationPath = '';
-  if (post.locationCountry) {
-    locationPath += post.locationCountry;
-  }
-  if (post.locationState) {
-    locationPath += `/${post.locationState}`;
-  }
-  if (post.locationCity) {
-    locationPath += `/${post.locationCity}`;
-  }
-  return locationPath;
-}
+  });
+  
+  exports.generateClonedPosts = functions.pubsub.schedule('every 24 hours').onRun(async () => {
+    const postPaths = [
+      "users/1ktzeQosrEOWFhKjKW5tMGXbfy22/posts/HP9bmPGps1NQufCy22SD",
+      "users/1ktzeQosrEOWFhKjKW5tMGXbfy22/posts/IJDGbsUdowDHftsE1tGZ",
+      "users/1ktzeQosrEOWFhKjKW5tMGXbfy22/posts/WRuRT0XZ3MWYSK04otkQ",
+      "users/1ktzeQosrEOWFhKjKW5tMGXbfy22/posts/u5J17e3m5PfXxNuOcNfR"
+    ];
+  
+    functions.logger.info('generateClonedPosts function started');
+  
+    try {
+      // Récupérer les détails des posts existants
+      functions.logger.info('Fetching details of existing posts');
+      const posts = await Promise.all(
+        postPaths.map(async (postPath) => {
+          const postRef = admin.firestore().doc(postPath);
+          const postDoc = await postRef.get();
+          if (postDoc.exists) {
+            functions.logger.info(`Post found with path: ${postPath}`);
+            return { ...postDoc.data(), id: postPath, authorRef: postDoc.ref };
+          } else {
+            functions.logger.error(`No post found with path: ${postPath}`);
+          }
+          return null;
+        })
+      );
+  
+      // Filtrer les posts null
+      const validPosts = posts.filter(post => post !== null);
+  
+      if (validPosts.length === 0) {
+        functions.logger.error('No valid posts found to clone');
+        return null;
+      }
+  
+      for (let i = 0; i < 10; i++) {
+        const originalPost = validPosts[i % validPosts.length]; // Récupérer un post en boucle
+        const newPostId = admin.firestore().collection("posts").doc().id;
+  
+        const newPost = {
+          ...originalPost,
+          caption: `${originalPost.caption} (cloned #${i + 1})`,
+          likes: 0,
+          date: admin.firestore.FieldValue.serverTimestamp(),
+        };
+  
+        const locationPostRef = admin.firestore().collection(`posts_man/France/posts`).doc(newPostId);
+  
+        await locationPostRef.set({
+          post_ref: originalPost.authorRef,
+          date: newPost.date,
+        });
+  
+        functions.logger.info(`Post cloned and added to ${locationPostRef.path}`);
+      }
+  
+      functions.logger.info('10 cloned posts created successfully');
+    } catch (error) {
+      console.error('Error creating cloned posts:', error);
+    }
+  });
