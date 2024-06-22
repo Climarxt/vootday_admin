32e1a347de84ce5942f982ca74afb0879676be45
« Christian » <christianbastide95@gmail.com>
2024-06-22 15:40:30 +0200

    Fix

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,53 @@
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-const cors = require('cors')({ origin: true });
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const { PubSub } = require('@google-cloud/pubsub');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+admin.initializeApp();
+const pubsub = new PubSub();
 
-exports.helloWorld = onRequest((request, response) => {
-  cors(request, response, () => {
-    logger.info("Hello logs!", { structuredData: true });
-    response.send("Hello from Firebase!");
-  });
+exports.scheduledDeleteSwipeManPosts_v1 = functions.pubsub.schedule('every 24 hours').onRun(async () => {
+  try {
+    const locationPath = 'France'; // Remplacez par la valeur appropriée
+    const postsSnapshot = await admin.firestore().collection(`swipe_man/${locationPath}/posts`).get();
+
+    if (postsSnapshot.empty) {
+      console.log('No posts found to delete');
+      return;
+    }
+
+    const batch = admin.firestore().batch();
+    postsSnapshot.forEach(doc => {
+      const docRef = admin.firestore().collection(`swipe_man/${locationPath}/posts`).doc(doc.id);
+      batch.delete(docRef);
+    });
+
+    await batch.commit();
+    console.log('Posts deleted successfully from swipe_man');
+
+    // Publier un message sur le sujet Pub/Sub après suppression réussie
+    const messageId = await pubsub.topic('posts-deleted').publish(Buffer.from('Posts deleted'));
+    console.log(`Message ${messageId} published.`);
+
+  } catch (error) {
+    console.error("Error deleting posts from swipe_man:", error);
+  }
 });
+
+exports.scheduledCopyPostsMan_v1 = functions.pubsub.topic('posts-deleted').onPublish(async () => {
+    try {
+      const locationPath = 'France'; // Remplacez par la valeur appropriée
+      const postsSnapshot = await admin.firestore().collection(`posts_man/${locationPath}/posts`).get();
+  
+      const batch = admin.firestore().batch();
+      postsSnapshot.forEach(doc => {
+        const newDocRef = admin.firestore().collection(`swipe_man/${locationPath}/posts`).doc(doc.id);
+        batch.set(newDocRef, doc.data());
+      });
+  
+      await batch.commit();
+      console.log('Posts copied successfully to swipe_man');
+    } catch (error) {
+      console.error("Error copying posts to swipe_man:", error);
+    }
+  });
+  
--- lib/screens/functions/functions_screen.dart
+++ lib/screens/functions/functions_screen.dart
@@ -1,5 +1,3 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:vootday_admin/config/configs.dart';
@@ -7,6 +5,11 @@ import 'package:vootday_admin/config/logger/logger.dart';
 
 class FunctionsScreen extends StatelessWidget {
   const FunctionsScreen({super.key});
+
+  final List<String> locationPaths = const [
+    'France',
+    // Ajoutez d'autres chemins de localisation si nécessaire
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -24,9 +27,9 @@ class FunctionsScreen extends StatelessWidget {
             SliverList(
               delegate: SliverChildBuilderDelegate(
                 (BuildContext context, int index) {
-                  return _buildBody(context)[index];
+                  return _buildBody(context, locationPaths)[index];
                 },
-                childCount: _buildBody(context).length,
+                childCount: _buildBody(context, locationPaths).length,
               ),
             ),
           ],
@@ -35,54 +38,63 @@ class FunctionsScreen extends StatelessWidget {
     );
   }
 
-  List<Widget> _buildBody(BuildContext context) {
-    return [
-      FunctionTile(
-        functionName: 'Function 1',
-        description: 'This is the description for function 1.',
+  List<Widget> _buildBody(BuildContext context, List<String> locationPaths) {
+    List<Widget> functionTiles = [];
+
+    for (String locationPath in locationPaths) {
+      functionTiles.add(FunctionTile(
+        functionName: 'Copy Posts Man for $locationPath',
+        description:
+            'Copy posts from posts_man/$locationPath/posts to swipe_man/$locationPath/posts.',
         onActivate: () async {
-          await _callFunction(context, 'Function 1');
+          await _callFunction(context, 'copyPostsMan', locationPath);
         },
-      ),
-      FunctionTile(
-        functionName: 'Function 2',
-        description: 'This is the description for function 2.',
+      ));
+
+      functionTiles.add(FunctionTile(
+        functionName: 'Copy Posts Woman for $locationPath',
+        description:
+            'Copy posts from posts_woman/$locationPath/posts to swipe_woman/$locationPath/posts.',
         onActivate: () async {
-          await _callFunction(context, 'Function 2');
+          await _callFunction(context, 'copyPostsWoman', locationPath);
         },
-      ),
-      FunctionTile(
-        functionName: 'Function 3',
-        description: 'This is the description for function 3.',
-        onActivate: () async {
-          await _callFunction(context, 'Function 3');
-        },
-      ),
-      // Add more FunctionTile widgets as needed
-    ];
+      ));
+    }
+
+    return functionTiles;
   }
 
-  Future<void> _callFunction(BuildContext context, String functionName) async {
+  Future<void> _callFunction(
+      BuildContext context, String functionName, String locationPath) async {
     final logger = ContextualLogger('FunctionsScreen._callFunction');
-    const url = 'https://us-central1-bootdv2.cloudfunctions.net/helloWorld';
+    final url =
+        'https://us-central1-bootdv2.cloudfunctions.net/$functionName?locationPath=$locationPath';
 
     try {
-      logger.logInfo('_callFunction', 'Calling cloud function', {'functionName': functionName, 'url': url});
+      logger.logInfo('_callFunction', 'Calling cloud function',
+          {'functionName': functionName, 'url': url});
       final response = await http.get(Uri.parse(url));
 
       if (response.statusCode == 200) {
-        logger.logInfo('_callFunction', 'Function activated successfully', {'functionName': functionName, 'response': response.body});
+        logger.logInfo('_callFunction', 'Function activated successfully',
+            {'functionName': functionName, 'response': response.body});
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Function $functionName activated successfully: ${response.body}')),
+          SnackBar(
+              content: Text(
+                  'Function $functionName activated successfully: ${response.body}')),
         );
       } else {
-        logger.logError('_callFunction', 'Failed to activate function', {'functionName': functionName, 'statusCode': response.statusCode});
+        logger.logError('_callFunction', 'Failed to activate function',
+            {'functionName': functionName, 'statusCode': response.statusCode});
         ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(content: Text('Failed to activate function $functionName: ${response.statusCode}')),
+          SnackBar(
+              content: Text(
+                  'Failed to activate function $functionName: ${response.statusCode}')),
         );
       }
     } catch (e) {
-      logger.logError('_callFunction', 'Error activating function', {'functionName': functionName, 'error': e.toString()});
+      logger.logError('_callFunction', 'Error activating function',
+          {'functionName': functionName, 'error': e.toString()});
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Error activating function $functionName: $e')),
       );
